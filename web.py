20971819250352402f25cba51253a1d2d8920754
mcabrerac23 <mcabrerac18@gmail.com>
2024-03-09 08:26:18 -0500

    text box moved to another place

--- web.py
+++ web.py
@@ -13,6 +13,9 @@ st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your <b>productivity<b>", unsafe_allow_html=True)
 
+st.text_input(label="New Jersey", placeholder="Add a new todo..",
+              on_change=add_todo, key='new_todo')
+
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -23,6 +26,4 @@ for index, todo in enumerate(todos):
         st.experimental_rerun()
 
 
-st.text_input(label="New Jersey", placeholder="Add a new todo..",
-              on_change=add_todo, key='new_todo')
 
