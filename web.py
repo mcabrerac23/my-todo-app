b2a287cd210ed800100f8a108115c6a2dc09f22a
mcabrerac23 <mcabrerac18@gmail.com>
2024-03-10 08:38:29 -0400

    Add configuration files

--- web.py
+++ web.py
@@ -12,7 +12,7 @@ st.set_page_config(layout="wide")
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("<h1>This app is to increase your <b>productivity<b><h1>", unsafe_allow_html=True)
+st.write("This app is to increase your <b>productivity<b>", unsafe_allow_html=True)
 
 
 for index, todo in enumerate(todos):
