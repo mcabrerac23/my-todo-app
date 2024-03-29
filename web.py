05f48f2e7523571fe38fc98cc7200db24853647d
mcabrerac23 <mcabrerac18@gmail.com>
2024-03-29 09:58:46 -0400

    updating aminated gif image and data for lima store to web.py

--- web.py
+++ web.py
@@ -1,12 +1,21 @@
 import streamlit as st
 import pandas
+import base64
 
 st.set_page_config(layout="wide")
 
 col1, = st.columns(1)
 
 with col1:
-    st.image("images/principal_stitch.png", width=300)
+    file_ = open("images/principal_stitch_animated.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+
+    st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+        unsafe_allow_html=True,
+    )
     st.title("IMPORTACION productos STITCH")
     content = """Productos STITCH - 2024
     """
@@ -21,8 +30,8 @@ col2, empty_col, col3, empty_col, col4 = \
 
 with col2:
     for index, row in df[:8].iterrows():
-        st.header(f'Code: :blue[{row["Codigo Producto"]}]')
-        st.subheader(row["Descripcion de Producto"])
+        st.header(row["Descripcion de Producto"])
+        st.subheader(f'Code: :blue[{row["Codigo Producto"]}]')
         st.write(f'Precio: :red[{row["Precio de Venta SOLES"]}]')
         st.write(f'Stock Disponible: {int(row["Stock Disponible"])}')
         st.image("images/" + row["image"])
@@ -30,16 +39,16 @@ with col2:
 
 with col3:
     for index, row in df[8:16].iterrows():
-        st.header(f'Code: :blue[{row["Codigo Producto"]}]')
-        st.subheader(row["Descripcion de Producto"])
+        st.header(row["Descripcion de Producto"])
+        st.subheader(f'Code: :blue[{row["Codigo Producto"]}]')
         st.write(f'Precio: :red[{row["Precio de Venta SOLES"]}]')
         st.write(f'Stock Disponible: {int(row["Stock Disponible"])}')
         st.image("images/" + row["image"])
 
 with col4:
     for index, row in df[16:24].iterrows():
-        st.header(f'Code: :blue[{row["Codigo Producto"]}]')
-        st.subheader(row["Descripcion de Producto"])
+        st.header(row["Descripcion de Producto"])
+        st.subheader(f'Code: :blue[{row["Codigo Producto"]}]')
         st.write(f'Precio: :red[{row["Precio de Venta SOLES"]}]')
         st.write(f'Stock Disponible: {int(row["Stock Disponible"])}')
         st.image("images/" + row["image"])
